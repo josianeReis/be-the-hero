7d7a65c24764e590c524b4f5c09a5a04c3d4cccd
JosianeReis <josiane-reis@outlook.com>
2020-05-08 13:24:01 -0300

    Fix estilos

--- be-the-hero-frontend/src/App.js
+++ be-the-hero-frontend/src/App.js
@@ -1,35 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-import Header from './Header';
 import Login from './pages/Login'
 import './global.css';
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> parent of f5b29f9... adicionando configs basicas
-
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Login />
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> parent of f5b29f9... adicionando configs basicas
     </div>
   );
 }
