f5b29f9259bdf6f5ffba5db72ba1d737a035fe59
JosianeReis <josiane.reis@morpheustecnologia.com>
2020-05-07 22:14:10 -0300

    adicionando configs basicas

---
+++ be-the-hero-frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function src() {
+  return (
+    <div>
+      <h1>be the hero</h1>
+    </div>
+  );
+}
+
+export default src;
---
+++ be-the-hero-frontend/src/pages/Login/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles';
+
+function Login() {
+  return <div />;
+}
+
+export default Login;
