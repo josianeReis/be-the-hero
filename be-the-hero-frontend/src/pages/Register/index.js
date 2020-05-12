c98d3ef25d68406a3dd7079bbbe479a5316cb46d
JosianeReis <josiane-reis@outlook.com>
2020-05-11 23:23:12 -0300

    adicionando pagina de cadastro

--- be-the-hero-frontend/src/pages/Register/index.js
+++ be-the-hero-frontend/src/pages/Register/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
 export default function Register() {
-  return <h1>Register</h1>;
+  return(
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero"/>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft gIn size={16} color="#E02041"/>
+            Não tenho cadastro
+          </Link>
+        </section>
+        <form>
+          <input placeholder="Nome da ONG" />
+          <input type="email" placeholder="E-mail" />
+          <input placeholder="WhatsApp" />
+          <div className="input-group">
+            <input placeholder="Cidade" />
+            <input placeholder="UF" style={{ width: 80 }}/>
+          </div>
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+
+      </div>
+    </div>
+  );
 }
